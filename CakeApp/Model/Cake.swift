c0d05dba17217b97a37eff5a91f780b0faa5ec00
MikeGAppX <mikael.galliot@appexperts.me.uk>
2021-10-10 12:10:44 +0100

    Added Model

---
+++ CakeApp/Model/Cake.swift
@@ -0,0 +1,14 @@
+//
+//  Cake.swift
+//  CakeApp
+//
+//  Created by Mikael Galliot on 10/10/2021.
+//
+
+import Foundation
+
+struct Cake: Codable {
+    let title: String
+    let desc: String
+    let image: String
+}
